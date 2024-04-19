3bac662322e1b0ada0ba509a9d72a58c6a4b9d6e
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-04-19 13:34:05 -0400

    Updated project files

--- main.js
+++ main.js
@@ -5,16 +5,35 @@
 // Input
 // Create and assign values to your variables
 
+const fullName = 'Dax Dewey';
+const date = '19 APR 2024';
+const project = 'JS Simple Calculator';
 
-
-
+// Get two numbers from the user using the prompt function
+const num1 = Number(prompt('Enter first number:'));
+const num2 = Number(prompt('Enter second number:'));
 
 // Process
 // Do the math for this project
-
-
-
+const additionAnswer = num1 + num2;
+const subtractionAnswer = num1 - num2;
+const multiplyAnswer = num1 * num2;
+const divideAnswer = num1 / num2;
 
 
 // Output
 // Show the results/answers
+console.log(fullName); 
+console.log(date);
+console.log(project);
+
+console.log('Output:');
+
+// Doing addition
+console.log(`${num1} + ${num2} = ${additionAnswer}`);
+// Doing subtraction
+console.log(`${num1} - ${num2} = ${subtractionAnswer}`);
+// Doing multiplication
+console.log(`${num1} * ${num2} = ${multiplyAnswer}`);
+//doing divide
+console.log(`${num1} / ${num2} = ${divideAnswer}`);
