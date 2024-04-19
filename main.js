4699a8cb8d853117dd326fd2ca23eab02960cf50
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-04-19 12:32:45 -0400

    Added calculator files

---
+++ main.js
@@ -0,0 +1,20 @@
+// Dax Dewey
+// 19 APR 2024
+// Evidence: JS Simple Calculator
+
+// Input
+// Create and assign values to your variables
+
+
+
+
+
+// Process
+// Do the math for this project
+
+
+
+
+
+// Output
+// Show the results/answers
